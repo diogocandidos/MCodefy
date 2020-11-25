6a2020d9fcbec153be7ad85d007b1a89440ee608
Diogo Candido <diogocandido.s@gmail.com>
2020-11-24 20:56:06 -0800

    1

---
+++ client/src/App.js
@@ -0,0 +1,138 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Wrapper from "./Components/Wrapper";
+import './Styles/Pure.css'
+import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+
+function App() {
+  return (
+    <Router>
+
+        <NavBar />
+        <Wrapper>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFoundPage} />
+        </Wrapper>
+        {/* <Footer /> */}
+   
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import "./App.css";
+// import Axios from "axios";
+
+// function App() {
+//   const [registerUsername, setRegisterUsername] = useState("");
+//   const [registerPassword, setRegisterPassword] = useState("");
+//   const [loginUsername, setLoginUsername] = useState("");
+//   const [loginPassword, setLoginPassword] = useState("");
+//   const [data, setData] = useState(null);
+//   const register = () => {
+//     Axios({
+//       method: "POST",
+//       data: {
+//         username: registerUsername,
+//         password: registerPassword,
+//       },
+//       withCredentials: true,
+//       url: "http://localhost:4000/register",
+//     }).then((res) => console.log(res));
+//   };
+//   const login = () => {
+//     Axios({
+//       method: "POST",
+//       data: {
+//         username: loginUsername,
+//         password: loginPassword,
+//       },
+//       withCredentials: true,
+//       url: "http://localhost:4000/login",
+//     }).then((res) => console.log(res));
+//   };
+//   const getUser = () => {
+//     Axios({
+//       method: "GET",
+//       withCredentials: true,
+//       url: "http://localhost:4000/user",
+//     }).then((res) => {
+//       setData(res.data);
+//       console.log(res.data);
+//     });
+//   };
+//   return (
+//     <div className="App">
+//       <div>
+//         <h1>Sign Up</h1>
+//         <input
+//           placeholder="username"
+//           onChange={(e) => setRegisterUsername(e.target.value)}
+//         />
+//         <input
+//           placeholder="password"
+//           onChange={(e) => setRegisterPassword(e.target.value)}
+//         />
+//         <button onClick={register}>Submit</button>
+//       </div>
+
+//       <div>
+//         <h1>Login</h1>
+//         <input
+//           placeholder="username"
+//           onChange={(e) => setLoginUsername(e.target.value)}
+//         />
+//         <input
+//           placeholder="password"
+//           onChange={(e) => setLoginPassword(e.target.value)}
+//         />
+//         <button onClick={login}>Submit</button>
+//       </div>
+
+//       <div>
+//         <h1>Get Started</h1>
+//         <button onClick={getUser}>Submit</button>
+//         {data ? <h1>Welcome Back {data.username}</h1> : null}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ server.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+const route = require("./routes/apiRoute")
+
+
+const PORT = process.env.PORT || 4000;
+//----------------------------------------- END OF IMPORTS---------------------------------------------------
+// Connect to the Mongo DB
+mongoose.connect(
+  "mongodb+srv://codefyLogin:CodefyJS@cluster0.abrzq.mongodb.net/codefy?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Mongoose is Connected to Codefy");
+  }
+);
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./Config/passportConfig")(passport);
+
+//----------------------------------------- END OF MIDDLEWARE---------------------------------------------------
+
+// ApiRoutes
+app.use(route);
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
